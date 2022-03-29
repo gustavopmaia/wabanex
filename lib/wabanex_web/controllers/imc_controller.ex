681deafa36b2ebbb68cffe2e4762ccba0c5adc48
Gustavo Maia <gustavopmaia@yahoo.com.br>
2022-03-29 20:48:06 -0300

    chore: Start project

---
+++ lib/wabanex_web/controllers/imc_controller.ex
@@ -0,0 +1,20 @@
+defmodule WabanexWeb.IMCController do
+  use WabanexWeb, :controller
+  alias Wabanex.IMC
+
+  def index(conn, params) do
+    params
+    |> IMC.calculate()
+    |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, result}, conn), do: render_response(conn, result, :ok)
+
+  defp handle_response({:error, reason}, conn), do: render_response(conn, reason, :bad_request)
+
+  defp render_response(conn, result, status) do
+    conn
+    |> put_status(status)
+    |> json(%{result: result})
+  end
+end
