c255f0a7ab52f41c8621f1137d630dd553eae80b
Gustavo Maia <gustavopmaia@yahoo.com.br>
2022-04-02 18:23:08 -0300

    chore: Show errors human readable

--- lib/wabanex_web/schema/types/root.ex
+++ lib/wabanex_web/schema/types/root.ex
@@ -2,6 +2,7 @@ defmodule WabanexWeb.Schema.Types.Root do
   use Absinthe.Schema.Notation
 
   alias WabanexWeb.Resolvers.User, as: UserResolver
+  alias Crudry.Middlewares.TranslateErrors
 
   import_types WabanexWeb.Schema.Types.User
 
@@ -18,6 +19,7 @@ defmodule WabanexWeb.Schema.Types.Root do
       arg :input, non_null(:create_user_input)
 
       resolve &UserResolver.create/2
+      middleware TranslateErrors
     end
   end
 end
