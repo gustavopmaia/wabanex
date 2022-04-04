0346f3e1822528e8b7380c9b0d6942f3ee8322ca
Gustavo Maia <gustavopmaia@yahoo.com.br>
2022-04-03 21:47:49 -0300

    feat: ExCoveralls

--- mix.exs
+++ mix.exs
@@ -10,7 +10,14 @@ defmodule Wabanex.MixProject do
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -47,7 +54,8 @@ defmodule Wabanex.MixProject do
       {:absinthe, "~> 1.6.0"},
       {:absinthe_plug, "~> 1.5"},
       {:argon2_elixir, "~> 3.0"},
-      {:crudry, "~> 2.4.0"}
+      {:crudry, "~> 2.4.0"},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
