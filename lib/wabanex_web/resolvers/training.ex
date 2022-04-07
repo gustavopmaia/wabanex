60c7555ce435e31e2d3d42be27efb98edce82848
Gustavo Maia <gustavopmaia@yahoo.com.br>
2022-04-07 17:27:54 -0300

    bugs: Fixing bugs

--- lib/wabanex_web/resolvers/training.ex
+++ lib/wabanex_web/resolvers/training.ex
@@ -1,3 +1,5 @@
 defmodule WabanexWeb.Resolvers.Training do
-  def create(%{input: params}, _context), do: Wabanex.Trainings.Create.call(params)
+  # def create(%{input: params}, _context), do: Wabanex.Trainings.Create.call(params)
+  def create(args, %{context: %{current_user: _current_user}}), do: Wabanex.Trainings.Create.call(args)
+  def create(_args, _info), do: {:error, "Not Authorized"}
 end
